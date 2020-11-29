cd335839ecab8e7a2eb0310bd46908d36f24da79
ayushi4 <38903413+ayushi4@users.noreply.github.com>
2020-11-29 20:03:03 +0530

    Create 0-1knapsack.py

---
+++ DP/0-1Knapsack/0-1knapsack.py
@@ -0,0 +1,41 @@
+
+# weight array and value array to maximize profit, within the capacity of knapsack
+wt = [1,3,4,5]
+val = [1,4,5,7]
+W = 7
+# n = len(wt)
+d = {}
+for i in range(len(wt)):
+    d[wt[i]] = val[i]     #hash-map to store wt and val
+print(d)
+# d = {
+#     1:1,
+#     3:4,
+#     4:5,
+#     5:7
+# }
+def knapsack(wt, val, W, d):
+    print(W)
+    if W==0:      #base condition 
+        return 0
+    
+    
+    if W in d:    # use of memoization to optimize time complexity
+        return d[W]
+    max_p = -10**9  
+    for w in wt:
+        if w <= W:
+            ans = knapsack(wt, val, W-w, d) + d[w]
+            max_p = max(max_p, ans)
+            # return max_p
+        # if w > W:
+        # else:
+        #     max_p = knapsack(wt, val, W)
+    # return max_p
+            
+    d[w] = max_p   #memoization
+    return max_p
+    
+max_profit =  knapsack(wt, val, W, d)
+print('-'*20)
+print(max_profit)
