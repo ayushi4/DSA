6ce05f9d86c18f99737592c7392afa66d3d6713a
ayushi4 <38903413+ayushi4@users.noreply.github.com>
2022-04-16 17:58:20 +0530

    Update missing_number_in_array.py

--- CheatCode/Math/missing_number_in_array.py
+++ CheatCode/Math/missing_number_in_array.py
@@ -1,8 +1,10 @@
 https://leetcode.com/problems/missing-number/
 
-#Gauss formula
 nums = [3,5,2,8,6,4,0,1]
-        
+
+#Gauss formula
+#Time Complexity: O(n)
+#Space Complexity: O(1)
 exp_sum = len(nums)*(len(nums)+1)//2
 acc_sum = 0
 
@@ -10,3 +12,17 @@ for i in nums:
     acc_sum += i
     
 print(exp_sum-acc_sum)
+
+
+
+#method-optimized: xor
+#Time Complexity: O(n)
+#Space Complexity: O(1)
+ans = 0
+for i in nums:
+    ans = ans^i
+
+for i in range(len(nums)+1):
+    ans = ans^i
+
+print(ans)
